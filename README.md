a79ebab7a5ee3cc984e9e37746619390cb18e0d2
nithin <nithinjosetom@gmail.com>
2016-05-08 11:52:48 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample scripts I've written using the pygame module. All scripts run on python3 (3.4 to be exact).
---
+++ hello_world.py
@@ -0,0 +1,12 @@
+import pygame, sys
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+
+window = pygame.display.set_mode((500, 400), 0, 32)
+pygame.
